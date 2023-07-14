4ef65f8219383c7143e9a08080071e8a3b11da30
Tomas Gomes <tomas.pc.gomes@gmail.com>
2023-07-14 12:28:48 +0100

    Added a function for basic Seurat QC

--- R/analysis_Seurat.R
+++ R/analysis_Seurat.R
@@ -1,5 +1,62 @@
 ##### STANDARD ANALYSIS #####
 
+# obtain basic Seurat QC metrics
+## setub mostly for human, and genes in ENS000-NAME format
+basicQCSeurat = function(obj, mt.pat = "-MT-", g.pat = c("","")){
+  # ribosomal genes
+  ## obtained from https://www.genenames.org/data/genegroup/#!/group/728
+  ## and https://www.genenames.org/data/genegroup/#!/group/729
+  rb_genes = c("RPLP0","RPLP1","RPLP2","RPL3","RPL3L","RPL4","RPL5","RPL6",
+               "RPL7","RPL7A","RPL7L1","RPL8","RPL9","RPL10","RPL10A","RPL10L","RPL11",
+               "RPL12","RPL13","RPL13A","RPL14","RPL15","RPL17","RPL18","RPL18A","RPL19",
+               "RPL21","RPL22","RPL22L1","RPL23","RPL23A","RPL24","RPL26","RPL26L1","RPL27",
+               "RPL27A","RPL28","RPL29","RPL30","RPL31","RPL32","RPL34","RPL35","RPL35A",
+               "RPL36","RPL36A","RPL36AL","RPL37","RPL37A","RPL38","RPL39","RPL39L","UBA52",
+               "RPL41","RPSA","RPS2","RPS3","RPS3A","RPS4X","RPS4Y1","RPS4Y2","RPS5","RPS6",
+               "RPS7","RPS8","RPS9","RPS10","RPS11","RPS12","RPS13","RPS14","RPS15",
+               "RPS15A","RPS16","RPS17","RPS18","RPS19","RPS20","RPS21","RPS23","RPS24",
+               "RPS25","RPS26","RPS27","RPS27A","RPS27L","RPS28","RPS29","FAU")
+  rb_genes_obj = unlist(sapply(rb_genes, 
+                               function(x) grep(paste0(g.pat[1],x,g.pat[2]), 
+                                                rownames(obj[["RNA"]]), 
+                                                value = T)))
+  
+  # scores
+  obj = Seurat::PercentageFeatureSet(obj, pattern = mt.pat, 
+                                     col.name = "percent.mt", assay = "RNA")
+  obj = Seurat::PercentageFeatureSet(obj, col.name = "percent.rb", 
+                                     assay = "RNA", features = rb_genes_obj)
+  
+  # cell cycle scoring
+  # might have trouble with bins so running iteratively until it works
+  s.genes = paste0(g.pat[1], cc.genes$s.genes, g.pat[2])
+  s.genes_obj = unlist(sapply(s.genes, 
+                              function(x) grep(x, rownames(obj[["RNA"]]), 
+                                               value = T)))
+  g2m.genes = paste0(g.pat[1], cc.genes$g2m.genes, g.pat[2])
+  g2m.genes_obj = unlist(sapply(g2m.genes, 
+                                function(x) grep(x, rownames(obj[["RNA"]]), 
+                                                 value = T)))
+  bins = 24
+  while(T){
+    objwsc = tryCatch({Seurat::CellCycleScoring(obj, nbin = bins, assay = "RNA",
+                                                s.features = s.genes_obj,
+                                                g2m.features = g2m.genes_obj,
+                                                set.ident = F)},
+                      error = function(e){print(bins)})
+    
+    if(is.numeric(objwsc) & bins>6){
+      bins = bins-2
+    } else{
+      obj = objwsc
+      break
+    }
+  }
+  
+  return(obj)
+}
+
+
 basicSeurat = function(obj){
   obj = suppressWarnings(SCTransform(obj, do.correct.umi = T, vars.to.regress = "nCount_RNA",
                                      verbose = F, variable.features.rv.th = 1,
