9771b9bec58ba61f8f9c15d4d678bd0b23f190ec
Tomas Gomes <tomas.pc.gomes@gmail.com>
2024-01-03 15:51:23 +0000

    Improved runSeuratClust

--- R/analysis_Seurat.R
+++ R/analysis_Seurat.R
@@ -92,14 +92,16 @@ basicSeurat = function(obj){
   return(obj)
 }
 
-runSeuratClust = function(obj, red = "pca", ncomp = 10){
+runSeuratClust = function(obj, red = "pca", ncomp = 10, max.res = 1.5, 
+                          algorithm = 2, ...){
   if(ncomp=="all"){ncomp = ncol(Embeddings(obj, red))}
   obj = FindNeighbors(obj, dims = 1:ncomp, force.recalc = T, verbose = F,
                       reduction = red, graph.name = paste0(red, ncomp))
   obj = RunUMAP(obj, dims = 1:ncomp, reduction = red, verbose = F)
-  obj = FindClusters(obj, algorithm = 2, verbose = F, graph.name = paste0(red, ncomp),
-                     resolution = seq(0.2, 1.5, 0.1))
-  # setting a more sensible identity as default
+  obj = FindClusters(obj, algorithm = algorithm, verbose = F, 
+                     graph.name = paste0(red, ncomp),
+                     resolution = seq(0.2, max.res, 0.1), ...)
+  # setting a low resolution as default
   obj = SetIdent(obj, value = paste0(red, ncomp, "_res.0.3"))
   return(obj)
 }
