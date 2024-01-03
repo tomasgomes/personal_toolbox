04cccbee9b1fde16337c6dd4c5296e9394dcab11
Tomas Gomes <tomas.pc.gomes@gmail.com>
2024-01-03 11:58:28 +0000

    Updated annotationPrompt

--- R/gene_expression.R
+++ R/gene_expression.R
@@ -4,7 +4,7 @@
 # example annotationPrompt(deg, n = 10, add_info = list("species" = "human", "tissue" = "liver"))
 annotationPrompt = function(presto_df, n = 10, 
                             pval_thr = 0.05, pct_diff = 20, logFC_thr = 0.2,
-                            add_info = NULL){
+                            add_info = NULL, print.it = T){
   require(dplyr)
   
   topmk = presto_df |>
@@ -12,22 +12,29 @@ annotationPrompt = function(presto_df, n = 10,
     filter(padj<=pval_thr) |> # only p-value below 0.05
     filter(pct_in>(pct_out+pct_diff)) |> 
     filter(logFC>logFC_thr) |> 
+    arrange(logFC) |>
     top_n(n = n, wt = logFC) # top genes per cluster, ranked by logFC
   
-  prompt = "I have performed a scRNA-seq analysis, and have encountered various clusters, for which I calculate the marker genes. The top marker genes for each cluster are as follows:\n"
+  prompt = "I have performed a scRNA-seq analysis, and have encountered various clusters, for which I calculated the marker genes. The top marker genes for each cluster are as follows:\n"
   for(cl in unique(topmk$group)){
     prompt = paste0(prompt, " - cluster ", cl, ": ", 
                     paste0(topmk$feature[topmk$group==cl], collapse = ", "), "\n")
   }
   
-  prompt = paste0(prompt, 
-                  "\nHere is more information about the data: ", 
-                  paste0(sapply(names(add_info), function(x) paste0(x, ": ", add_info[x])), 
-                         collapse = "; "), "\n")
+  if(!is.null(add_info)){
+    prompt = paste0(prompt, 
+                    "\nTo interpret these clusters, these additional sample characteristics should be considered: ", 
+                    paste0(sapply(names(add_info), function(x) paste0(x, ": ", add_info[x])), 
+                           collapse = "; "), "\n")
+  }
   
   prompt = paste0(prompt, 
-                  "Can you tell me what are the most likely cell types that each cluster matches to?")
-  cat(prompt)
+                  "Can you tell me what are the most likely cell types that each cluster matches to? And report the top 5 genes used to reach this annotation.")
+  
+  if(print.it){
+    cat(prompt)
+  }
+  return(prompt)
 }
 
 
